852518ff9600be419816d6ca17ddac0207041adf
Matthew Nally <matthewnally1@gmail.com>
2023-06-24 14:46:46 +0100

    sway updates

--- nvim/lua/matthew/set.lua
+++ nvim/lua/matthew/set.lua
@@ -16,7 +16,7 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
@@ -29,5 +29,4 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 vim.cmd("highlight ColorColumn ctermbg=235 guibg=#262626")
-
 vim.g.netrw_liststyle = 3
