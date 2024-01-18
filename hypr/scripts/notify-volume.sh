22b987f8bcd999407c3bda9059cdb3765b5be63a
Matthew Nally <matthewnally1@gmail.com>
2024-01-18 20:36:41 +0000

    volume update

--- hypr/scripts/notify-volume.sh
+++ hypr/scripts/notify-volume.sh
@@ -6,33 +6,34 @@ msgTag="myvolume"
 
 # Change the volume using alsa(might differ if you use pulseaudio)
 # amixer -c 0 set Master "$@" > /dev/null
-mute="$(pactl get-sink-mute @DEFAULT_SINK@ | sed 's/.* //g')"
-volume="$(pactl get-sink-volume @DEFAULT_SINK@ | head -n 1| awk '{print substr($5, 1, length($5)-1)}')"
+mute="$(pamixer --get-mute)"
+volume="$(pamixer --get-volume)"
 if [[ "$1" == "up" && $volume -lt 100 ]] then
-    if [[ "$mute" == "yes" ]] then
-	pactl set-sink-mute @DEFAULT_SINK@ toggle
+    if [[ "$mute" == "true" ]] then
+	pamixer --unmute
     fi
 
-    pactl set-sink-volume @DEFAULT_SINK@ +5%
+    pamixer -i 5
 elif [[ "$1" == "down" ]] then
-    if [[ "$mute" == "yes" ]] then
-	pactl set-sink-mute @DEFAULT_SINK@ toggle
+    if [[ "$mute" == "true" ]] then
+	pamixer --unmute
     fi
-    pactl set-sink-volume @DEFAULT_SINK@ -5%
+    pamixer -d 5
 elif [[ "$1" == "mute" ]] then
-    pactl set-sink-mute @DEFAULT_SINK@ toggle
+    pamixer --toggle-mute
 fi
 
 # Query amixer for the current volume and whether or not the speaker is muted
-volume="$(pactl get-sink-volume @DEFAULT_SINK@ | head -n 1| awk '{print substr($5, 1, length($5)-1)}')"
+# volume="$(pactl get-sink-volume @DEFAULT_SINK@ | head -n 1| awk '{print substr($5, 1, length($5)-1)}')"
+volume="$(pamixer --get-volume)"
 # volume="$(amixer -c 0 get Master | tail -1 | awk '{print $4}' | sed 's/[^0-9]*//g')"
 # mute="$(amixer -c 0 get Master | tail -1 | awk '{print $6}' | sed 's/[^a-z]*//g')"
 # mute="$(pactl get-sink-volume @DEFAULT_SINK@ | head -n 1 | awk '{print substr($5, 1, length($5)-1)}')"
-mute="$(pactl get-sink-mute @DEFAULT_SINK@ | sed 's/.* //g')"
-if [[ $volume == 0 || "$mute" == "yes" ]]; then
+mute="$(pamixer --get-mute)"
+if [[ $volume == 0 || "$mute" == "true" ]]; then
     # Show the sound muted notification
     dunstify -a "changeVolume" -u low -i /usr/share/icons/Adwaita/symbolic/status/audio-volume-muted-symbolic.svg  -h string:x-dunst-stack-tag:$msgTag \
-    -h int:value:"$volume" "Muted"
+    -h int:value:"0" "Muted"
     else
     # Show the volume notification
     dunstify -a "changeVolume" -u low -i /usr/share/icons/Adwaita/symbolic/status/audio-volume-medium-symbolic.svg -h string:x-dunst-stack-tag:$msgTag \
