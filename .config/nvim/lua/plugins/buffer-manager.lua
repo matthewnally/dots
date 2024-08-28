1cca86e496db25ff6422c28b75d6315c0fc06c0b
Matthew Nally <matthewnally1@gmail.com>
2024-08-28 12:58:27 +0100

    nvim

--- .config/nvim/lua/plugins/buffer-manager.lua
+++ .config/nvim/lua/plugins/buffer-manager.lua
@@ -1,9 +1,9 @@
 return {
     "j-morano/buffer_manager.nvim",
     config = function()
-        local bm = require('buffer_manager')
-        vim.keymap.set("n", "<leader>b", function() bm.ui.toggle_quick_menu() end, { desc = "Buffer Manager" })
-        vim.keymap.set("n", "<tab>", function() bm.ui.nav_next(1) end)
-        vim.keymap.set("n", "<s-tab>", function() bm.ui.nav_prev(1) end)
+        local bmui = require('buffer_manager.ui')
+        vim.keymap.set("n", "<leader>b", function() bmui.toggle_quick_menu() end, { desc = "Buffer Manager" })
+        vim.keymap.set("n", "<tab>", function() bmui.nav_next(1) end)
+        vim.keymap.set("n", "<s-tab>", function() bmui.nav_prev(1) end)
     end
 }
